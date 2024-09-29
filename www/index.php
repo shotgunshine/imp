0928577669eba02db1e7622b180a3442018fccc6
shotgunshine <mc517053@gmail.com>
2024-09-29 18:08:52 +0200

    fix document root

--- www/index.php
+++ www/index.php
@@ -6,13 +6,14 @@ session_start([
 	'cookie_samesite' => 'Strict',
 ]);
 
+$root = __DIR__ . '/../';
 $uri = explode('/', parse_url($_SERVER['REQUEST_URI'])['path']);
 
 if ($uri[1] == '' or $uri[1] == 'm') {
-	require 'lang/lang.php';
-	require 'view/head.phtml';
-	require 'view/predictor.phtml';
-	require 'view/foot.phtml';
+	require $root . 'lang/lang.php';
+	require $root . 'view/head.phtml';
+	require $root . 'view/predictor.phtml';
+	require $root . 'view/foot.phtml';
 }
 
 elseif ($uri[1] == 'team') {
@@ -22,14 +23,14 @@ elseif ($uri[1] == 'team') {
 		} else {
 			$teamId = '';
 		}
-		require 'chpp/match.php';
+		require $root . 'chpp/match.php';
 		$matches = getMatches($teamId);
 		if ($matches->Team->MatchList->children()) {
-			require 'lang/lang.php';
+			require $root . 'lang/lang.php';
 			$title = htmlspecialchars($matches->Team->TeamName);
-			require 'view/head.phtml';
-			require 'view/team.phtml';
-			require 'view/foot.phtml';
+			require $root . 'view/head.phtml';
+			require $root . 'view/team.phtml';
+			require $root . 'view/foot.phtml';
 		} else {
 			if ($teamId === '') {
 				header('Location: /logout', true, 302);
@@ -43,7 +44,7 @@ elseif ($uri[1] == 'team') {
 }
 
 elseif ($uri[1] == 'login') {
-	require 'chpp/auth.php';
+	require $root . 'chpp/auth.php';
 	$response = getParameters(obtainRequestToken());
 	if (isset($response['oauth_token'])) {
 		$_SESSION['requestToken'] = $response['oauth_token'];
@@ -52,17 +53,17 @@ elseif ($uri[1] == 'login') {
 		$path .= '&oauth_callback=https://' . $_SERVER['SERVER_NAME'] . '/request_token_ready';
 		header('Location: ' . $path, true, 302);
 	} else {
-		require 'lang/lang.php';
+		require $root . 'lang/lang.php';
 		$title = getTranslation('errorChppTitle');
-		require 'view/head.phtml';
+		require $root . 'view/head.phtml';
 		$error = getTranslation('errorChppMessage');
-		require 'view/error.phtml';
-		require 'view/foot.phtml';
+		require $root . 'view/error.phtml';
+		require $root . 'view/foot.phtml';
 	}
 }
 
 elseif ($uri[1] == 'request_token_ready') {
-	require 'chpp/auth.php';
+	require $root . 'chpp/auth.php';
 	if (isset($_GET['oauth_verifier']) and strstr($_SERVER['HTTP_REFERER'], paths['authorizeToken'])) {
 		$response = getParameters(obtainAccessToken(
 			htmlspecialchars($_GET['oauth_verifier']),
@@ -73,7 +74,7 @@ elseif ($uri[1] == 'request_token_ready') {
 			$_SESSION['accessToken'] = $response['oauth_token'];
 			$_SESSION['accessSecret'] = $response['oauth_token_secret'];
 			$_SESSION['teams'] = [];
-			require 'chpp/match.php';
+			require $root . 'chpp/match.php';
 			foreach (getTeams()->Teams->children() as $t) {
 				$teamId = $t->TeamID->__toString();
 				$teamName = $t->ShortTeamName->__toString();
@@ -91,9 +92,9 @@ elseif ($uri[1] == 'logout') {
 
 elseif ($uri[1] == 'match') {
 	if (isset($_SESSION['accessToken'])) {
-		require 'chpp/match.php';
+		require $root . 'chpp/match.php';
 		if (boolval($_GET['timeline'])) {
-			$cache = 'cache/' . intval($_GET['match_id']) . '.json';
+			$cache = $root . 'cache/' . intval($_GET['match_id']) . '.json';
 			if (file_exists($cache)) {
 				header('Cache-Control: public, max-age=31536000');
 				echo file_get_contents($cache);
@@ -123,10 +124,10 @@ elseif ($uri[1] == 'match') {
 
 else {
 	header('HTTP/1.0 404 Not found');
-	require 'lang/lang.php';
+	require $root . 'lang/lang.php';
 	$title = getTranslation('errorNotFoundTitle');
-	require 'view/head.phtml';
+	require $root . 'view/head.phtml';
 	$error = getTranslation('errorNotFoundMessage');
-	require 'view/error.phtml';
-	require 'view/foot.phtml';
+	require $root . 'view/error.phtml';
+	require $root . 'view/foot.phtml';
 }
